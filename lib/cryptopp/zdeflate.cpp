083348d0e8e2e38db0b890fe8f58052f94763bdc
caohongjia <caohongjia@users.noreply.github.com>
2026-09-17 07:52:01 +0000

    ci: avoid stdext iterators in cryptopp zdeflate on modern MSVC

--- lib/cryptopp/zdeflate.cpp
+++ lib/cryptopp/zdeflate.cpp
@@ -396,7 +396,7 @@ unsigned int Deflator::LongestMatch(unsigned int &bestMatch) const
 #else
 				std::mismatch
 #endif
-#if _MSC_VER >= 1600
+#if 0
 				(stdext::make_unchecked_array_iterator(scan)+3, stdext::make_unchecked_array_iterator(scanEnd), stdext::make_unchecked_array_iterator(match)+3).first - stdext::make_unchecked_array_iterator(scan));
 #else
 				(scan+3, scanEnd, match+3).first - scan);
